12856d5fab85917f86850fed0ffe05e6654a603d
Jayakumar33 <jayak@lambdatest.com>
2024-07-18 19:48:58 +0530

    Updated code to appium

--- src/test/java/driver/driver/DriverFactory.java
+++ src/test/java/driver/driver/DriverFactory.java
@@ -2,64 +2,59 @@ package driver.driver;
 
 import com.thoughtworks.gauge.AfterSpec;
 import com.thoughtworks.gauge.BeforeSpec;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverFactory {
 
-    // Get a new WebDriver Instance.
-    // There are various implementations for this depending on browser/OS. The
-    // required browser/OS can be set as an environment variable.
-    // Refer
-    // http://getgauge.io/documentation/user/current/managing_environments/README.html
+    private static final String LT_USERNAME = System.getenv("LT_USERNAME");
+    private static final String LT_ACCESS_KEY = System.getenv("LT_ACCESS_KEY");
+    private static final String GRID_URL = "@mobile-hub.lambdatest.com/wd/hub";
 
-    private static final String username = System.getenv("LT_USERNAME");
-    private static final String accesskey = System.getenv("LT_ACCESS_KEY");
-    public static final String gridURL = "@hub.lambdatest.com/wd/hub";
-    public String status = "passed";
+    private static AppiumDriver driver;
 
-    private  WebDriver driver = null;
-
-    public  WebDriver getDriver() {
-        if(driver==null)
-             this.setUp();
+    public static AppiumDriver getDriver() {
+        if (driver == null) {
+            setUpDriver();
+        }
         return driver;
     }
 
-    @BeforeSpec
-    public void setUp() {
-
+    private static void setUpDriver() {
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("browserName", System.getenv("BROWSER"));
-            capabilities.setCapability("version", System.getenv("BROWSER_VERSION"));
-            capabilities.setCapability("platform", System.getenv("PLATFORM"));
-            capabilities.setCapability("build", "Java Gauge Framework");
-            capabilities.setCapability("name", "Sample Gauge Test");
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "Galaxy S21 5G");
+            capabilities.setCapability("platformVersion", "11");
+            capabilities.setCapability("app", "lt://proverbial-android");
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("build", "Gauge-Appium-Java");
+            capabilities.setCapability("name", "Sample Script");
             capabilities.setCapability("network", false); // To enable network logs
-            capabilities.setCapability("visual", false); // To enable step by step screenshot
+            capabilities.setCapability("visual", true); // To enable step by step screenshot
             capabilities.setCapability("console", false); // To capture console logs
 
-            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+            String url = "https://" + LT_USERNAME + ":" + LT_ACCESS_KEY + GRID_URL;
+            driver = new AppiumDriver<>(new URL(url), capabilities);
         } catch (MalformedURLException e) {
-            System.out.println("Invalid grid URL");
+            System.out.println("Invalid app URL");
+            e.printStackTrace();
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
-
     }
 
     @AfterSpec
     public void tearDown() {
         if (driver != null) {
-            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + "passed");
             driver.quit();
         }
     }
-
 }
--- src/test/java/driver/driver/StepImplementation_ToDo.java
+++ src/test/java/driver/driver/StepImplementation_ToDo.java
@@ -1,6 +1,8 @@
 package driver.driver;
 
 import com.thoughtworks.gauge.Step;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,41 +13,74 @@ import static org.junit.Assert.assertEquals;
 
 public class StepImplementation_ToDo {
 
-    private final WebDriver driver;
+
+    private AppiumDriver<MobileElement> driver;
 
     public StepImplementation_ToDo() {
-        this.driver = new DriverFactory().getDriver();
+        this.driver = DriverFactory.getDriver();
     }
 
     @Step("Open the todo app")
     public void gotoApp() throws InterruptedException {
 
         System.out.println(this.driver);
-        driver.get("https://lambdatest.github.io/sample-todo-app/");
-        String title = driver.getTitle();
-        assertEquals(title, "Sample page - lambdatest.com");
     }
 
     @Step("Select the desired items")
     public void selectItems() throws InterruptedException {
 
-        WebElement itemOne = driver.findElement(By.name("li1"));
-        itemOne.click();
+        MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
+        //Changes color to pink
+        color.click();
+        Thread.sleep(1000);
+        //Back to orginal color
+        color.click();
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        MobileElement text = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/Text");
+        //Changes the text to "Proverbial"
+        text.click();
 
-        WebElement itemThird = driver.findElement(By.name("li3"));
-        itemThird.click();
+        //toast will be visible
+        MobileElement toast = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/toast");
+        toast.click();
+
+        //notification will be visible
+        MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
+        notification.click();
+        Thread.sleep(2000);
+
+        //Opens the geolocation page
+        MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
+        geo.click();
+        Thread.sleep(5000);
+
+        //takes back to home page
+        MobileElement home = (MobileElement) driver.findElementByAccessibilityId("Home");
+        home.click();
+
     }
 
     @Step("Add new item <itemName>")
     public void addNewItem(String itemName) throws InterruptedException {
 
-        WebElement addItem = driver.findElement(By.id("sampletodotext"));
-        addItem.sendKeys(itemName);
-        Thread.sleep(2000);
 
-        WebElement addButton = driver.findElement(By.id("addbutton"));
-        addButton.click();
+        //Takes to speed test page
+        MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
+        speedtest.click();
+        Thread.sleep(5000);
+
+        MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
+        Home.click();
+
+        //Opens the browser
+        MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
+        browser.click();
+
+        MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
+        url.sendKeys("https://www.lambdatest.com");
+
+        MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
+        find.click();
+        driver.quit();
     }
 }
